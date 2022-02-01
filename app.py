6402d7d25cb1f7350f659cca4d8b5da96249b7b6
namanbajpayi <bajpayinaman@gmail.com>
2022-02-01 20:12:04 +0530

    adding all files

---
+++ app.py
@@ -0,0 +1,92 @@
+from flask import Flask, jsonify, request
+from datetime import datetime as dt
+import pytz
+created_at=''
+modified_at=''
+app = Flask("myapp")
+IST = pytz.timezone('Asia/Kolkata')
+
+tasks=[]
+count=1
+err = {"error": "Invalid task id"}
+emp = {"error":"No task is Present, Db is empty"}
+
+@app.route('/task/<int:n>', methods=['GET','PUT','DELETE'])
+def rest(n):   
+    if request.method == 'GET':
+        if len(tasks)==0:
+            return jsonify(emp)
+        for i in range(len(tasks)):
+            if tasks[i]['id']==n:
+                return jsonify(tasks[i])        
+        return jsonify(err)
+
+    if request.method == 'DELETE':
+        if len(tasks)==0:
+            return jsonify(emp)
+        for i in range(len(tasks)):
+            if tasks[i]['id']==n:
+                tasks.pop(i)
+                return jsonify({"msg":"done"})        
+        return jsonify(err)
+
+    if request.method == 'PUT':    
+        global modified_at
+        stamp= dt.now(IST)
+        data = request.get_json(force = True) 
+        print(data)  
+        modified_at= stamp.strftime('%Y-%m-%d-%H-%M-%S')    
+        if len(tasks) == 0:
+            return jsonify(emp)
+        for i in range(len(tasks)):
+            if tasks[i]['id']==n: 
+                tasks[i]["task_name"]=data["task_name"]
+                tasks[i]["modified_at"]=modified_at  
+                return jsonify(tasks[i])       
+        return jsonify(err)     
+
+@app.route('/task', methods=['GET','POST'])
+def rest2():
+        global count
+        global created_at
+        global modified_at
+        x={"msg":"done"}
+        y={"msg":"task already present"}
+        if request.method == 'GET':
+            if len(tasks)==0:
+                emp={"error":"No task is Present, Db is empty"}
+                return jsonify(emp)     
+            else:
+                return jsonify(tasks)       
+        if request.method == 'POST':  
+            stamp= dt.now(IST)
+            data = request.get_json()
+            created_at= stamp.strftime('%Y-%m-%d-%H-%M-%S')
+            modified_at= stamp.strftime('%Y-%m-%d-%H-%M-%S')                        
+            if len(tasks) == 0:
+                count=1
+                result={
+                    "id":count,
+                    "task_name": data["task_name"],
+                    "created_at": created_at,
+                    "modified_at": modified_at
+                    }
+                count=count+1 
+                tasks.append(result) 
+                return jsonify(x)                
+            for i in range(len(tasks)):
+                if tasks[i]["task_name"]==data["task_name"]:    
+                    return jsonify(y)
+            result={
+                "id":count,
+                "task_name": data["task_name"],
+                "created_at": created_at,
+                "modified_at": modified_at
+                }
+            count=count+1 
+            tasks.append(result)  
+            return jsonify(x) 
+        
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ dockerfile
@@ -0,0 +1,12 @@
+FROM alpine:latest
+
+RUN apk update
+RUN apk add py-pip
+RUN apk add --no-cache python3-dev 
+RUN pip3 install --upgrade pip
+
+WORKDIR /app
+COPY . /app
+RUN pip --no-cache-dir install -r requirements.txt
+
+CMD ["python3", "app.py"]
